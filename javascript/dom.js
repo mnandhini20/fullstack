5e3d0dda52e6f3fecc3db629ef2280969774c611
mnandhini20 <146616199+mnandhini20@users.noreply.github.com>
2024-02-17 10:10:02 +0530

    Add files via upload

---
+++ javascript/dom.js
@@ -0,0 +1,7 @@
+var division = document.querySelector("#select");
+
+// Use firstChild to get the first child node of the selected element
+var get = division.firstChild;
+
+// Log the result to the console
+console.log(get);
